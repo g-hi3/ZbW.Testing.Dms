8730da71c4502378226c43360e8f67c7c3b6391f
Marc Forrer <marc.forrer@bluewin.ch>
2019-09-21 14:22:36 +0200

    Set up all tests for Test ID 1259

---
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Tests/ApplicationNavigationTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace ZbW.Testing.Dms.Tests
+{
+    public class ApplicationNavigationTests
+    {
+        [Test]
+        public void OpenSearchView_OnSearchButtonClick()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        
+        [Test]
+        public void OpenDocumentDetailView_OnAddButtonClick()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        
+        [Test]
+        public void ShowUserName_UserIsLoggedIn()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        
+        public int TestId => 1259;
+    }
+}
